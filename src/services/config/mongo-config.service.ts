8a6b462e6e6065b40a12da46f66a5b9d90609fca
Qubo Qin <quboqin0710@gmail.com>
2024-07-04 13:41:51 +0800

    remove console log

--- src/services/config/mongo-config.service.ts
+++ src/services/config/mongo-config.service.ts
@@ -2,7 +2,6 @@ import { MongooseOptionsFactory, MongooseModuleOptions } from '@nestjs/mongoose'
 
 export class MongoConfigService implements MongooseOptionsFactory {
   createMongooseOptions(): MongooseModuleOptions {
-    console.log('MONGO_DSN', process.env.MONGO_DSN)
     return {
       uri: process.env.MONGO_DSN
     }
