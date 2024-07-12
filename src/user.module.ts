37e6f3589c883858d2365131d0a1e245a7570949
Qubo Qin <quboqin0710@gmail.com>
2024-07-12 19:47:13 +0800

    console db connect

--- src/user.module.ts
+++ src/user.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common'
+import { Module, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
 import { ClientProxyFactory } from '@nestjs/microservices'
 import { MongooseModule } from '@nestjs/mongoose'
 import { UserController } from './user.controller'
@@ -7,6 +7,7 @@ import { MongoConfigService } from './services/config/mongo-config.service'
 import { ConfigService } from './services/config/config.service'
 import { UserSchema } from './schemas/user.schema'
 import { UserLinkSchema } from './schemas/user-link.schema'
+import mongoose from 'mongoose'
 
 @Module({
   imports: [
@@ -40,4 +41,39 @@ import { UserLinkSchema } from './schemas/user-link.schema'
     }
   ]
 })
-export class UserModule {}
+export class UserModule implements OnModuleInit, OnModuleDestroy {
+  private readonly MAX_RETRIES = 5
+  private retries = 0
+
+  onModuleInit() {
+    console.log('UserModule initialized')
+    this.connectWithRetry()
+  }
+
+  onModuleDestroy() {
+    mongoose.connection.close()
+  }
+
+  private connectWithRetry() {
+    mongoose
+      .connect(process.env.MONGO_DSN)
+      .then(() => {
+        console.log('Mongoose connected successfully')
+        this.retries = 0 // reset retries on successful connection
+      })
+      .catch((err) => {
+        console.error('Mongoose connection error:', err)
+        if (this.retries < this.MAX_RETRIES) {
+          this.retries += 1
+          console.log(`Retrying connection attempt ${this.retries}/${this.MAX_RETRIES}...`)
+          setTimeout(this.connectWithRetry.bind(this), 5000) // wait 5 seconds before retrying
+        } else {
+          console.error('Max retries reached. Could not connect to MongoDB.')
+        }
+      })
+
+    mongoose.connection.on('disconnected', () => {
+      console.log('Mongoose disconnected')
+    })
+  }
+}
